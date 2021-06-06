f07e7fe8f6ff61aef7c5a178c9ed788d27cf7186
unknown <fizz.zehra14@gmail.com>
2021-06-06 01:01:27 -0500

    style

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,7 +16,7 @@ var questions = [{
 },
 {
     Que: "Can we assign null to void pointer?",
-    Opt: ["Yes","No"],
+    Opt: ["Yes", "No"],
     Ans: "Yes( )"
 },
 {
@@ -35,177 +35,177 @@ var timer;
 //when user clicks start button timer should be started
 function start() {
 
-timeLeft = questions.length*15;
-document.querySelector("#timeLeft").textContent = timeLeft;
-
-timer = setInterval(function() {
-    timeLeft--;
+    timeLeft = questions.length * 15;
     document.querySelector("#timeLeft").textContent = timeLeft;
-    //proceed to end the game function when timer is below 0 at any time
-    if (timeLeft <= 0) {
-        clearInterval(timer);
-        endGame(); 
-    }
-}, 1000);
 
-next();
+    timer = setInterval(function () {
+        timeLeft--;
+        document.querySelector("#timeLeft").textContent = timeLeft;
+        //proceed to end the game function when timer is below 0 at any time
+        if (timeLeft <= 0) {
+            clearInterval(timer);
+            endGame();
+        }
+    }, 1000);
+
+    next();
 }
 
 //stop the timer to end the game 
 function endGame() {
-clearInterval(timer);
+    clearInterval(timer);
 
-var quiEl = document.getElementById("quizBody");
+    var quiEl = document.getElementById("quizBody");
 
-var h2El=document.createElement("h2");
-h2El.textContent="Game over!";
-quiEl.append(h2El);
+    var h2El = document.createElement("h2");
+    h2El.textContent = "Game over!";
+    quiEl.append(h2El);
 
-var h3El=document.createElement("h3");
-h3El.textContent="You got a " + score +  "/100!<";
-quiEl.append(h3El);
+    var h3El = document.createElement("h3");
+    h3El.textContent = "You got a " + score + "/100!<";
+    quiEl.append(h3El);
 
-var h3El2=document.createElement("h3");
-h3El2.textContent="That means you got " + score / 20 +  " questions correct";
-quiEl.append(h3El2);
+    var h3El2 = document.createElement("h3");
+    h3El2.textContent = "That means you got " + score / 20 + " questions correct";
+    quiEl.append(h3El2);
 
-var Firstname=document.createElement("input");
-Firstname.setAttribute("type", "text");
-Firstname.setAttribute("id", "name");
+    var Firstname = document.createElement("input");
+    Firstname.setAttribute("type", "text");
+    Firstname.setAttribute("id", "name");
 
-Firstname.placeholder="First name"
-quiEl.append(Firstname);
+    Firstname.placeholder = "First name"
+    quiEl.append(Firstname);
 
-//set score button element
-var scoreBtn=scoreBtn=document.createElement("Button");
-scoreBtn.setAttribute("id","setscore")
-scoreBtn.textContent="Set score";
-quiEl.append(scoreBtn);
+    //set score button element
+    var scoreBtn = scoreBtn = document.createElement("Button");
+    scoreBtn.setAttribute("id", "setscore")
+    scoreBtn.textContent = "Set score";
+    quiEl.append(scoreBtn);
 
-scoreBtn.addEventListener("click", function () {
-    setScore();
-});
+    scoreBtn.addEventListener("click", function () {
+        setScore();
+    });
 }
 function setScore() {
     localStorage.setItem("highscore", score);
-    localStorage.setItem("playerName",  document.getElementById('name').value);
+    localStorage.setItem("playerName", document.getElementById('name').value);
     getScore();
-    }
-    
-    
+}
+
+
 //store the scores on local storage
 
 function getScore() {
 
-var quizBodyEl = document.getElementById("quizBody");
-var h2=document.createElement("h2");
-h2.textContent=localStorage.getItem("playerName") + "'s highscore is:";
-quizBodyEl.append(h2);
-var h1=document.createElement("h1");
-h1.textContent=localStorage.getItem("highscore") ;
-quizBodyEl.append(h1);
+    var quizBodyEl = document.getElementById("quizBody");
+    var h2 = document.createElement("h2");
+    h2.textContent = localStorage.getItem("playerName") + "'s highscore is:";
+    quizBodyEl.append(h2);
+    var h1 = document.createElement("h1");
+    h1.textContent = localStorage.getItem("highscore");
+    quizBodyEl.append(h1);
 }
 //clears the score name and value in the local storage if the user selects 'clear score'
 function clearScore() {
-localStorage.setItem("highscore", "");
-localStorage.setItem("playerName",  "");
-resetGame();
+    localStorage.setItem("highscore", "");
+    localStorage.setItem("playerName", "");
+    resetGame();
 }
 
 //reset the game 
 function resetGame() {
-clearInterval(timer);
-score = 0;
-currentQuestion = -1;
-timeLeft = 0;
-timer = null;
+    clearInterval(timer);
+    score = 0;
+    currentQuestion = -1;
+    timeLeft = 0;
+    timer = null;
 
-document.getElementById("timeLeft").innerHTML = timeLeft;
+    document.getElementById("timeLeft").innerHTML = timeLeft;
 
 
 
-var quizContent=document.getElementById("quizBody");
-var h1=document.createElement('h1');
-h1.textContent="Coding Quiz!"
-quizContent.append(h1);
-var h3=document.createElement('h3');
-h3.textContent="Click Start to play!"
-quizContent.append(h3);
+    var quizContent = document.getElementById("quizBody");
+    var h1 = document.createElement('h1');
+    h1.textContent = "Coding Quiz!"
+    quizContent.append(h1);
+    var h3 = document.createElement('h3');
+    h3.textContent = "Click Start to play!"
+    quizContent.append(h3);
 
-//start the quiz
-var startBtn=document.createElement("Button");
-startBtn.textContent="Start Quiz";
-quizbody.append(startBtn);
+    //start the quiz
+    var startBtn = document.createElement("Button");
+    startBtn.textContent = "Start Quiz";
+    quizbody.append(startBtn);
 
-startBtn.addEventListener("click", function () {
-    start();
-});
+    startBtn.addEventListener("click", function () {
+        start();
+    });
 
-document.getElementById("quizBody").innerHTML = quizContent;
+    document.getElementById("quizBody").innerHTML = quizContent;
 }
 
 //deduct 15seconds from the timer if user chooses an incorrect answer
 function incorrect() {
-timeLeft -= 15; 
-document.getElementById("quizBody").innerHTML=" ";
-next();
+    timeLeft -= 15;
+    document.getElementById("quizBody").innerHTML = " ";
+    next();
 }
 
 //increases the score by 20points if the user chooses the correct answer
 function correct() {
-score += 20;
-document.getElementById("quizBody").innerHTML=" ";
-next();
+    score += 20;
+    document.getElementById("quizBody").innerHTML = " ";
+    next();
 }
 
 //loops through the questions 
 function next() {
-currentQuestion++;
+    currentQuestion++;
 
-if (currentQuestion > questions.length - 1) {
-    endGame();
-    return;
-}
-else{
-var quizContent = document.getElementById("quizBody");
-var h2=document.createElement("h2");
-h2.textContent=questions[currentQuestion].Que;
-quizContent.append(h2);
-var ol=document.createElement("ol");
-ol.setAttribute("id","Optionslist")
-quizContent.append(ol);
-
-for (var i = 0; i < questions[currentQuestion].Opt.length; i++) {
-    var buttonCode = "<button onclick=\"[ANS]\">[CHOICE]</button>"; 
-    buttonCode = buttonCode.replace("[CHOICE]", questions[currentQuestion].Opt[i]);
-    if (questions[currentQuestion].Opt[i] == questions[currentQuestion].Ans) {
-        buttonCode = buttonCode.replace("[ANS]", "correct()");
-    } else {
-        buttonCode = buttonCode.replace("[ANS]", "incorrect()");
+    if (currentQuestion > questions.length - 1) {
+        endGame();
+        return;
     }
-    var li=document.createElement("li");
-    li.innerHTML=buttonCode;
-    ol.append(li);
-    quizContent.append(ol);
-}
-}
+    else {
+        var quizContent = document.getElementById("quizBody");
+        var h2 = document.createElement("h2");
+        h2.textContent = questions[currentQuestion].Que;
+        quizContent.append(h2);
+        var ol = document.createElement("ol");
+        ol.setAttribute("id", "Optionslist")
+        quizContent.append(ol);
+
+        for (var i = 0; i < questions[currentQuestion].Opt.length; i++) {
+            var buttonCode = "<button onclick=\"[ANS]\">[CHOICE]</button>";
+            buttonCode = buttonCode.replace("[CHOICE]", questions[currentQuestion].Opt[i]);
+            if (questions[currentQuestion].Opt[i] == questions[currentQuestion].Ans) {
+                buttonCode = buttonCode.replace("[ANS]", "correct()");
+            } else {
+                buttonCode = buttonCode.replace("[ANS]", "incorrect()");
+            }
+            var li = document.createElement("li");
+            li.innerHTML = buttonCode;
+            ol.append(li);
+            quizContent.append(ol);
+        }
+    }
 
 }
 
 //body
-var quizbody=document.createElement('div');
-quizbody.setAttribute("id","quizBody")
+var quizbody = document.createElement('div');
+quizbody.setAttribute("id", "quizBody")
 document.querySelector("#quiz").append(quizbody);
-var h1=document.createElement('h1');
-h1.textContent="Coding Quiz!"
+var h1 = document.createElement('h1');
+h1.textContent = "Coding Quiz!"
 quizbody.append(h1);
-var h3=document.createElement('h3');
-h3.textContent="Click Start to "
+var h3 = document.createElement('h3');
+h3.textContent = "Click Start to "
 quizbody.append(h3);
 
 //start the quiz
-var startBtn=document.createElement("Button");
-startBtn.textContent="Start Quiz";
+var startBtn = document.createElement("Button");
+startBtn.textContent = "Start Quiz";
 quizbody.append(startBtn);
 
 startBtn.addEventListener("click", function () {
@@ -213,24 +213,24 @@ startBtn.addEventListener("click", function () {
 });
 
 //view highscores link on the top left of nav bar-->
-var navLeft=document.createElement('div');
-navLeft.setAttribute("class","left")
+var navLeft = document.createElement('div');
+navLeft.setAttribute("class", "left")
 document.querySelector("#nav").append(navLeft);
-var a=document.createElement('a');
-quizbody.setAttribute("href","#")
-a.addEventListener("click",getScore());
+var a = document.createElement('a');
+a.setAttribute("href", "#")
+a.addEventListener("click", getScore());
 navLeft.append(a);
-var h4=document.createElement('h4');
-h4.textContent="High Scores";
+var h4 = document.createElement('h4');
+h4.textContent = "High Scores";
 a.append(h4);
 
 //countdown timer on the top right of nav bar-->
-var navRight=document.createElement('div');
-navRight.setAttribute("class","right")
+var navRight = document.createElement('div');
+navRight.setAttribute("class", "right")
 document.querySelector("#nav").append(navRight);
-var h42=document.createElement('h42');
-h42.textContent="Timer :\t";
+var h42 = document.createElement('h42');
+h42.textContent = "Timer :";
 navRight.append(h42);
-var span=document.createElement('span');
-span.setAttribute("id","timeLeft")
+var span = document.createElement('span');
+span.setAttribute("id", "timeLeft")
 h42.append(span)
